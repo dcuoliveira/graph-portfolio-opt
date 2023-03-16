18d83479ad6ce9df600d8813b18402dba3ace1e1
Daniel Cunha Oliveira <dan.cunhaa@gmail.com>
2023-03-16 14:37:24 -0300

    add training procedure

--- src/learning/optimization.py
+++ src/learning/optimization.py
@@ -5,8 +5,12 @@ def run_training_procedure(file, model, init_steps, prediction_steps):
     
     data = pd.read_excel(file)
     
+    # 1. use in-sample data to estimate graph structure
+ 
+    # 2. use graph structure and/or time series data to predict realized covariances
     rcovs = []
     preds = []
+    forecast_errors = []
     for t in tqdm(range(init_steps, data.shape[0] - init_steps, prediction_steps),
                   desc="Running TSCV"):
 
@@ -25,3 +29,8 @@ def run_training_procedure(file, model, init_steps, prediction_steps):
 
         # store t+1 realized covariance prediction
         preds.append(None)
+        
+        # compute forecast error
+        
+        # store forecast error
+        forecast_errors.append(None)
