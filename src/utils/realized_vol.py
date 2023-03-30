e2ffb98f50935be088e1d2ba0383dc519d8575e2
dcuoliveira <dan.cunhaa@gmail.com>
2023-03-30 13:14:31 -0300

    fix rcov

--- src/utils/realized_vol.py
+++ src/utils/realized_vol.py
@@ -1,6 +1,19 @@
-def compute_rcov(data,
-                 rvol_proxy_name,
-                 rvol_proxy_window):
-    
+import torch
+
+def compute_rcov(returns,
+                 rvol_proxy_name):
+    T = returns.shape[0]
+    k = returns.shape[1]
+
+    rcov = torch.zeros(T, k, k)
     if rvol_proxy_name == "sample_cov":
-        data = data.rolling(window=rvol_proxy_window).cov()
+        for t in range(returns.shape[0]):
+            rts = torch.tensor(returns.iloc[t])
+
+            rcov_t = torch.kron(rts, rts).reshape(k, k)
+            rcov[t] = rcov_t
+    else:
+        raise Exception("Realized covariance proxy '{}' not implemented!!".format(rvol_proxy_name))
+
+    return rcov
+
