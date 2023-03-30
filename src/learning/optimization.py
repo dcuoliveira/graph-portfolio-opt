55f6f3b02ecb5db51ed90ffc57dc93ef540dc88d
dcuoliveira <dan.cunhaa@gmail.com>
2023-03-30 13:14:52 -0300

    add stable version

--- src/learning/optimization.py
+++ src/learning/optimization.py
@@ -1,54 +1,59 @@
 import pandas as pd
 from tqdm import tqdm
+import numpy as np
+import torch
+import torch.nn.functional as F
 
 from utils.realized_vol import compute_rcov
+from learning.forecast import forecast
 
 def run_training_procedure(file,
                            model,
                            model_name,
                            rvol_proxy_name,
-                           rvol_proxy_window,
                            init_steps,
                            prediction_steps,
                            embedd_init_steps):
     
-    data = pd.read_excel(file)
+    prices = pd.read_excel(file)
     
     # 0. prepare dataset
-    data.set_index("date", inplace=True)
-    data = data.dropna()
+    prices.set_index("date", inplace=True)
+    prices = prices
+    returns = np.log(prices).diff().dropna()
 
-    # 1. compute realized vol. proxy
-    rcov_data = compute_rcov(data=data, rvol_proxy_name=rvol_proxy_name, rvol_proxy_window=rvol_proxy_window)
-    
-    # 2. use in-sample data to estimate graph embeddings
-    embedd_data = data.iloc[0:embedd_init_steps]
-    data = data.iloc[embedd_init_steps+1:]
+    # 1. use in-sample data to estimate graph embeddings
+    embedd_returns = returns.iloc[0:embedd_init_steps]
+    returns = returns.iloc[embedd_init_steps+1:]
+
+    # 2. compute realized vol. proxy
+    rcovs_true = compute_rcov(returns=returns, rvol_proxy_name=rvol_proxy_name)
     
     # 3. use graph structure and/or time series data to predict realized covariances
-    rcovs = []
-    preds = []
-    forecast_errors = []
-    for t in tqdm(range(init_steps, data.shape[0] - init_steps, prediction_steps),
+    T = (returns.shape[0] - init_steps)
+    k = returns.shape[1]
+    idx = 0
+
+    preds = losses = torch.zeros(T, k, k)
+    for t in tqdm(range(init_steps, returns.shape[0] - init_steps, prediction_steps),
                   desc="Running TSCV"):
 
         # get 1:t ts information
-        train = data.iloc[:t]
-        test = data.iloc[t:(t + prediction_steps)]
+        train = returns.iloc[:t]
+        test = returns.iloc[t:(t + prediction_steps)]
 
-        # compute realized covariance
-
-        # store realized covariance
-        rcovs.append(None)
-
-        # estimate model parameters
-
-        # compute t+1 predictions
+        # estimate model parameters and compute t+1 predictions
+        rcov_tp1 = forecast(returns=train, model=model, model_name=model_name, prediction_steps=prediction_steps)
 
         # store t+1 realized covariance prediction
-        preds.append(None)
+        preds[idx] = rcov_tp1
         
         # compute forecast error
+        loss = F.mse_loss(input=rcov_tp1, target=rcovs_true[t])
         
         # store forecast error
-        forecast_errors.append(None)
+        losses[idx] = loss
+
+        idx += 1
+    
+    a = 1
