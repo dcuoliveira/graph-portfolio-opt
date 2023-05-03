f15e35ce8a0f83c5181007c9c884d6299dcfe38b
dcuoliveira <dan.cunhaa@gmail.com>
2023-05-03 07:28:11 -0300

    first commit

---
+++ src/utils/dataset_utils.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+def concatenate_prices_returns(prices, returns):
+    prices_names = list(prices.columns)
+    returns_names = list(returns.columns)
+
+    names = list(set(prices_names) & set(returns_names))
+    
+    all = []
+    for name in names:
+        all.append(prices[[name]].rename(columns={name: "{} price".format(name)}))
+        all.append(returns[[name]].rename(columns={name: "{} ret".format(name)}))
+    all_df = pd.concat(all, axis=1)
+
+    return all_df
