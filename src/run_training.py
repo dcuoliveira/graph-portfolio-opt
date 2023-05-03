1ff461b287a88712a1550087187d6fd79e476d3e
dcuoliveira <dan.cunhaa@gmail.com>
2023-05-03 07:11:04 -0300

    delete unused parameters

--- src/run_training.py
+++ src/run_training.py
@@ -12,21 +12,17 @@ inputs_path = os.path.join(source_path, "data", "inputs")
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--ds_name', type=str, default="etfs", help='Dataset name.')
-parser.add_argument('--model_name', type=str, default="rw", help='Model name.')
-parser.add_argument('--rvol_proxy_name', type=str, default="sample_cov", help='Realized vol. proxy name to be computed.')
-parser.add_argument('--init_steps', type=int, default=252, help='Init steps to estimate before predicting.')
+parser.add_argument('--model_name', type=str, default="dlpo", help='Model name.')
+parser.add_argument('--init_steps', type=int, default=252, help='Init steps to estimate model before predicting.')
 parser.add_argument('--prediction_steps', type=int, default=1, help='Steps ahead to predict.')
-parser.add_argument('--embedd_init_steps', type=int, default=252, help='Steps to estimate graph embeddings.')
 
 if __name__ == "__main__":
     args = parser.parse_args()
     results = run_training_procedure(file=os.path.join(inputs_path, ds_names[args.ds_name]),
                                      model=model_names[args.model_name],
-                                     rvol_proxy_name=args.rvol_proxy_name,
                                      model_name=args.model_name,
                                      init_steps=args.init_steps,
-                                     prediction_steps=args.prediction_steps,
-                                     embedd_init_steps=args.embedd_init_steps)
+                                     prediction_steps=args.prediction_steps)
     
     if not os.path.isdir(os.path.join(OUTPUTS_PATH, args.model_name)):
         os.mkdir(os.path.join(OUTPUTS_PATH, args.model_name))
