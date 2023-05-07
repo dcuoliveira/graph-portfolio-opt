6b662841bdd6a0b3321ca3401d5b160ae2aa5ceb
dcuoliveira <dan.cunhaa@gmail.com>
2023-05-07 10:19:43 -0300

    stable fnunct window version

--- src/utils/dataset_utils.py
+++ src/utils/dataset_utils.py
@@ -1,4 +1,22 @@
 import pandas as pd
+import torch
+import numpy as np
+
+
+def create_rolling_window_array(data, seq_length):
+    
+    X = []
+    y = []
+    for i in range(len(data) - seq_length - 1):
+        _X = data[i:(i + seq_length)]
+        _y = data[(i + seq_length)]
+        X.append(_X)
+        y.append(_y)
+
+    y_out = torch.Tensor(y)
+    X_out = torch.Tensor(X)
+
+    return X_out, y_out
 
 def concatenate_prices_returns(prices, returns):
     prices_names = list(prices.columns)
