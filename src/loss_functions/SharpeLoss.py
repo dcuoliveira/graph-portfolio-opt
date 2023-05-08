f8af585d8ec9534775e1d57ebc8fc553e25c7225
dcuoliveira <dan.cunhaa@gmail.com>
2023-05-08 19:59:23 -0300

    commit

---
+++ src/loss_functions/SharpeLoss.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+
+class SharpeLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, returns, vol, weights):
+        
+        realized_sharpe_ratio = torch.divide(torch.mul(weights, returns), vol)
+
+        return realized_sharpe_ratio
