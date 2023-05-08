05334b1b7de6e1dbeee6185c5e64bced5798b85b
dcuoliveira <dan.cunhaa@gmail.com>
2023-05-08 20:50:42 -0300

    add realized vol

--- src/utils/conn_data.py
+++ src/utils/conn_data.py
@@ -1,4 +1,11 @@
 import pickle
+import numpy as np
+
+def compute_realized_ewma_vol(returns, window=50):
+    
+    ewma_vol_df = (returns.ewm(window).std() * np.sqrt(252))
+
+    return ewma_vol_df
 
 def save_pickle(path: str,
                 obj: dict):
