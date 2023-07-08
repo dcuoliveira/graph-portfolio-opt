005df2860692c3c1cf742861bfa81d19abab2baf
dcuoliveira <dan.cunhaa@gmail.com>
2023-07-08 19:14:20 -0300

    fix graph window function

--- src/utils/dataset_utils.py
+++ src/utils/dataset_utils.py
@@ -22,7 +22,14 @@ def create_rolling_indices(num_timesteps_in, num_timesteps_out, n_timesteps, fix
     return indices
 
 def create_rolling_window_ts(target, features, num_timesteps_in, num_timesteps_out, fix_start=False):
-    
+    """"
+    This function is used to create the rolling window time series to be used on DL ex-GNN.
+
+    One important thing to note is that, since we are in the context of sharpe ratio optimization,
+    and we are assuming positions are being taken at the close price of the same day, we have to 
+    increase our target variable (prices) by one time step so as to compute the sharpe ratio properly.
+    """
+        
     if features.shape[0] != target.shape[0]:
         raise Exception("Features and target must have the same number of timesteps")
 
@@ -44,7 +51,13 @@ def create_rolling_window_ts(target, features, num_timesteps_in, num_timesteps_o
     return window_features, window_target
 
 def create_rolling_window_ts_for_graphs(target, features, num_timesteps_in, num_timesteps_out, fix_start=False):
-    
+    """"
+    This function is used to create the rolling window time series to be used on GNNs.
+
+    One important thing to note is that, since we are in the context of sharpe ratio optimization,
+    and we are assuming positions are being taken at the close price of the same day, we have to 
+    increase our target variable (prices) by one time step so as to compute the sharpe ratio properly.
+    """
     if features.shape[-1] != target.shape[-1]:
         raise Exception("Features and target must have the same number of timesteps")
 
@@ -58,7 +71,7 @@ def create_rolling_window_ts_for_graphs(target, features, num_timesteps_in, num_
     window_features, window_target = [], []
     for i, j in indices:
         window_features.append((features[:, :, i : i + num_timesteps_in]).numpy())
-        window_target.append((target[:, i + num_timesteps_in : j]).numpy())
+        window_target.append((target[:, (i + num_timesteps_in - 1):j]).numpy())
 
     return window_features, window_target
 
