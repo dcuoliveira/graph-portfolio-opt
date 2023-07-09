28bda3538d2ecb02e20626711cd54f95ccdc2e96
dcuoliveira <dan.cunhaa@gmail.com>
2023-07-09 18:54:11 -0300

    add args

--- src/run_dlpo.py
+++ src/run_dlpo.py
@@ -4,6 +4,7 @@ import torch.utils.data as data
 import torch
 from tqdm import tqdm
 import argparse
+import json
 
 from utils.dataset_utils import create_rolling_window_ts, timeseries_train_test_split
 from loss_functions.SharpeLoss import SharpeLoss
@@ -48,161 +49,170 @@ parser.add_argument('-mn',
                     help='model name to be used for saving the model',
                     default="dlpo")
 
-args = parser.parse_args()
+if __name__ == "__main__":
 
-model_name = args.model_name
+    args = parser.parse_args()
 
-# neural network hyperparameters
-input_size = 1426
-output_size = 1426
-hidden_size = 64
-num_layers = 1
+    model_name = args.model_name
 
-# optimization hyperparameters
-learning_rate = 1e-3
+    # neural network hyperparameters
+    input_size = 1426
+    output_size = 1426
+    hidden_size = 64
+    num_layers = 1
 
-# training hyperparameters
-device = torch.device('cpu')
-epochs = args.epochs
-batch_size = args.batch_size
-drop_last = True
-num_timesteps_in = args.num_timesteps_in
-num_timesteps_out = args.num_timesteps_out
-train_ratio = 0.5
-ascent = True
-fix_start=False
-train_shuffle = args.train_shuffle
+    # optimization hyperparameters
+    learning_rate = 1e-3
 
-# relevant paths
-source_path = os.path.dirname(__file__)
-inputs_path = os.path.join(source_path, "data", "inputs")
+    # training hyperparameters
+    device = torch.device('cpu')
+    epochs = args.epochs
+    batch_size = args.batch_size
+    drop_last = True
+    num_timesteps_in = args.num_timesteps_in
+    num_timesteps_out = args.num_timesteps_out
+    train_ratio = 0.5
+    ascent = True
+    fix_start=False
+    train_shuffle = args.train_shuffle
 
-# prepare dataset
-loader = NewETFs(use_last_data=True)
-prices = loader.y.T
-features = loader.X
-features = features.reshape(features.shape[0], features.shape[1] * features.shape[2]).T
+    # relevant paths
+    source_path = os.path.dirname(__file__)
+    inputs_path = os.path.join(source_path, "data", "inputs")
 
-# define train and test datasets
-X_train, X_val, prices_train, prices_val = timeseries_train_test_split(features, prices, train_ratio=train_ratio)
-X_val, X_test, prices_val, prices_test = timeseries_train_test_split(X_val, prices_val, train_ratio=0.5) 
+    # prepare dataset
+    loader = NewETFs(use_last_data=True)
+    prices = loader.y.T
+    features = loader.X
+    features = features.reshape(features.shape[0], features.shape[1] * features.shape[2]).T
 
-X_train, prices_train = create_rolling_window_ts(features=X_train, 
-                                                 target=prices_train,
-                                                 num_timesteps_in=num_timesteps_in,
-                                                 num_timesteps_out=num_timesteps_out,
-                                                 fix_start=fix_start)
-X_val, prices_val = create_rolling_window_ts(features=X_val, 
-                                             target=prices_val,
-                                             num_timesteps_in=num_timesteps_in,
-                                             num_timesteps_out=num_timesteps_out,
-                                             fix_start=fix_start)
-X_test, prices_test = create_rolling_window_ts(features=X_test, 
-                                               target=prices_test,
-                                               num_timesteps_in=num_timesteps_in,
-                                               num_timesteps_out=num_timesteps_out,
-                                               fix_start=fix_start)
+    # define train and test datasets
+    X_train, X_val, prices_train, prices_val = timeseries_train_test_split(features, prices, train_ratio=train_ratio)
+    X_val, X_test, prices_val, prices_test = timeseries_train_test_split(X_val, prices_val, train_ratio=0.5) 
 
-# define data loaders
-train_loader = data.DataLoader(data.TensorDataset(X_train, prices_train), shuffle=train_shuffle, batch_size=batch_size, drop_last=drop_last)
-val_loader = data.DataLoader(data.TensorDataset(X_val, prices_val), shuffle=False, batch_size=batch_size, drop_last=drop_last)
-test_loader = data.DataLoader(data.TensorDataset(X_test, prices_test), shuffle=False, batch_size=batch_size, drop_last=drop_last)
+    X_train, prices_train = create_rolling_window_ts(features=X_train, 
+                                                    target=prices_train,
+                                                    num_timesteps_in=num_timesteps_in,
+                                                    num_timesteps_out=num_timesteps_out,
+                                                    fix_start=fix_start)
+    X_val, prices_val = create_rolling_window_ts(features=X_val, 
+                                                target=prices_val,
+                                                num_timesteps_in=num_timesteps_in,
+                                                num_timesteps_out=num_timesteps_out,
+                                                fix_start=fix_start)
+    X_test, prices_test = create_rolling_window_ts(features=X_test, 
+                                                target=prices_test,
+                                                num_timesteps_in=num_timesteps_in,
+                                                num_timesteps_out=num_timesteps_out,
+                                                fix_start=fix_start)
 
-# (1) model
-model = DLPO(input_size=input_size,
-             output_size=output_size,
-             hidden_size=hidden_size,
-             num_layers=num_layers,
-             batch_first=True,
-             num_timesteps_out=num_timesteps_out).to(device)
+    # define data loaders
+    train_loader = data.DataLoader(data.TensorDataset(X_train, prices_train), shuffle=train_shuffle, batch_size=batch_size, drop_last=drop_last)
+    val_loader = data.DataLoader(data.TensorDataset(X_val, prices_val), shuffle=False, batch_size=batch_size, drop_last=drop_last)
+    test_loader = data.DataLoader(data.TensorDataset(X_test, prices_test), shuffle=False, batch_size=batch_size, drop_last=drop_last)
 
-# (2) loss fucntion
-lossfn = SharpeLoss()
+    # (1) model
+    model = DLPO(input_size=input_size,
+                output_size=output_size,
+                hidden_size=hidden_size,
+                num_layers=num_layers,
+                batch_first=True,
+                num_timesteps_out=num_timesteps_out).to(device)
 
-# (3) optimizer
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    # (2) loss fucntion
+    lossfn = SharpeLoss()
 
-# (4) training procedure
-train_loss_values = eval_loss_values = test_loss_values = []
-pbar = tqdm(range(epochs + 1), total=(epochs + 1))
-for epoch in pbar:
+    # (3) optimizer
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-    # train model
-    model.train()
-    train_loss = 0
-    for X_batch, prices_batch in train_loader:
-                
-        # compute forward propagation
-        weights_pred = model.forward(X_batch)
+    # (4) training procedure
+    train_loss_values = eval_loss_values = test_loss_values = []
+    pbar = tqdm(range(epochs + 1), total=(epochs + 1))
+    for epoch in pbar:
 
-        # compute loss
-        loss = lossfn(prices_batch, weights_pred, ascent=ascent)
-        train_loss += loss.detach().item() * -1
-
-        # compute gradients and backpropagate
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
-
-    # store average train loss
-    avg_train_loss = train_loss / len(train_loader)
-    train_loss_values.append(avg_train_loss)
-
-    # evaluate model 
-    model.eval()
-    with torch.no_grad():
-        eval_loss = 0
-        for X_batch, prices_batch in val_loader:
-            
+        # train model
+        model.train()
+        train_loss = 0
+        for X_batch, prices_batch in train_loader:
+                    
             # compute forward propagation
             weights_pred = model.forward(X_batch)
 
             # compute loss
             loss = lossfn(prices_batch, weights_pred, ascent=ascent)
-            eval_loss += loss.detach().item() * -1
-    
-        # store average evaluation loss
-        avg_eval_loss = eval_loss / len(val_loader)
-        eval_loss_values.append(avg_eval_loss)
+            train_loss += loss.detach().item() * -1
 
-    pbar.set_description("Epoch: %d, Train sharpe : %1.5f, Eval sharpe : %1.5f" % (epoch, avg_train_loss, avg_eval_loss))
+            # compute gradients and backpropagate
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
 
-train_loss_df = pd.DataFrame(train_loss_values, columns=["sharpe_ratio"])
-eval_loss_df = pd.DataFrame(eval_loss_values, columns=["sharpe_ratio"])
+        # store average train loss
+        avg_train_loss = train_loss / len(train_loader)
+        train_loss_values.append(avg_train_loss)
 
-with torch.no_grad():
-    # compute train weight predictions
-    weights_pred = model.forward(X_train)
-    # select predictions
-    weights_train_pred = weights_pred[:, -num_timesteps_out, :]
+        # evaluate model 
+        model.eval()
+        with torch.no_grad():
+            eval_loss = 0
+            for X_batch, prices_batch in val_loader:
+                
+                # compute forward propagation
+                weights_pred = model.forward(X_batch)
 
-    # compute val weight predictions
-    weights_pred = model.forward(X_val)
-    # select predictions
-    weights_eval_pred = weights_pred[:, -num_timesteps_out, :]
+                # compute loss
+                loss = lossfn(prices_batch, weights_pred, ascent=ascent)
+                eval_loss += loss.detach().item() * -1
+        
+            # store average evaluation loss
+            avg_eval_loss = eval_loss / len(val_loader)
+            eval_loss_values.append(avg_eval_loss)
 
-    # compute test weight predictions
-    weights_pred = model.forward(X_test)
-    # select predictions
-    weights_test_pred = weights_pred[:, -num_timesteps_out, :]
+        pbar.set_description("Epoch: %d, Train sharpe : %1.5f, Eval sharpe : %1.5f" % (epoch, avg_train_loss, avg_eval_loss))
 
-results = {
-    
-    "model": model.state_dict(),
-    "train_loss": train_loss_df,
-    "eval_loss": eval_loss_df,
-    "test_loss": avg_eval_loss,
+    train_loss_df = pd.DataFrame(train_loss_values, columns=["sharpe_ratio"])
+    eval_loss_df = pd.DataFrame(eval_loss_values, columns=["sharpe_ratio"])
 
-    }
+    with torch.no_grad():
+        # compute train weight predictions
+        weights_pred = model.forward(X_train)
+        # select predictions
+        weights_train_pred = weights_pred[:, -num_timesteps_out, :]
 
-output_path = os.path.join(os.getcwd(),
-                           "src,"
-                           "data",
-                           "outputs",
-                           model_name)
-output_name = "{model_name}_{epochs}_{batch_size}_{num_timesteps_in}_{num_timesteps_out}.pt".format(model_name=model_name,
-                                                                                                    epochs=epochs,
-                                                                                                    batch_size=batch_size,
-                                                                                                    num_timesteps_in=num_timesteps_in,
-                                                                                                    num_timesteps_out=num_timesteps_out)
-torch.save(results, os.path.join(output_path, output_name))
+        # compute val weight predictions
+        weights_pred = model.forward(X_val)
+        # select predictions
+        weights_eval_pred = weights_pred[:, -num_timesteps_out, :]
+
+        # compute test weight predictions
+        weights_pred = model.forward(X_test)
+        # select predictions
+        weights_test_pred = weights_pred[:, -num_timesteps_out, :]
+
+    results = {
+        
+        "model": model.state_dict(),
+        "train_loss": train_loss_df,
+        "eval_loss": eval_loss_df,
+        "test_loss": avg_eval_loss,
+
+        }
+
+    output_path = os.path.join(os.getcwd(),
+                            "src,"
+                            "data",
+                            "outputs",
+                            model_name)
+
+    # save args
+    args_dict = vars(args)  
+    with open(os.path.join(output_path, 'args.json'), 'w') as fp:
+        json.dump(args_dict, fp)
+
+    # save results
+    output_name = "{model_name}_{epochs}_{batch_size}_{num_timesteps_in}_{num_timesteps_out}.pt".format(model_name=model_name,
+                                                                                                        epochs=epochs,
+                                                                                                        batch_size=batch_size,
+                                                                                                        num_timesteps_in=num_timesteps_in,
+                                                                                                        num_timesteps_out=num_timesteps_out)
+    torch.save(results, os.path.join(output_path, output_name))
