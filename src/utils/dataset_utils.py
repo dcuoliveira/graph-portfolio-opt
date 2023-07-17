490ab1128eb07d6cb5d7f76328d01a9af6444d31
dcuoliveira <dan.cunhaa@gmail.com>
2023-07-16 21:34:16 -0300

    create online features

--- src/utils/dataset_utils.py
+++ src/utils/dataset_utils.py
@@ -1,13 +1,23 @@
 import pandas as pd
 import torch
 
+def timeseries_train_test_split_online(X, y, test_size):
+    T = (X.shape[0] - test_size)
+
+    X_train = X[:T, :]
+    y_train = y[:T, :]
+    X_test = X[:T, :]
+    y_test = y[(T-1):, :]
+
+    return X_train, X_test, y_train, y_test
+
 def timeseries_train_test_split(X, y, train_ratio):
-    train_ratio = int(len(X) * train_ratio)
+    train_size = int(len(X) * train_ratio)
     
-    X_train = X[:train_ratio, :]
-    y_train = y[:train_ratio, :]
-    X_test = X[train_ratio:, :]
-    y_test = y[train_ratio:, :]
+    X_train = X[:train_size, :]
+    y_train = y[:train_size, :]
+    X_test = X[train_size:, :]
+    y_test = y[train_size:, :]
 
     return X_train, X_test, y_train, y_test
 
@@ -33,6 +43,36 @@ def create_rolling_indices(num_timesteps_in, num_timesteps_out, n_timesteps, fix
                 indices.append((indices[-1][0] + num_timesteps_out,  indices[-1][1] + num_timesteps_out))
 
     return indices
+
+def create_online_rolling_window_ts(target, features, num_timesteps_in, num_timesteps_out, fix_start=False, drop_last=True):
+    """"
+    This function is used to create the rolling window time series to be used on DL ex-GNN.
+
+    One important thing to note is that, since we are in the context of sharpe ratio optimization,
+    and we are assuming positions are being taken at the close price of the same day, we have to 
+    increase our target variable (prices) by one time step so as to compute the sharpe ratio properly.
+    """
+        
+    if features.shape[0] != target.shape[0]:
+        raise Exception("Features and target must have the same number of timesteps")
+
+    n_timesteps = features.shape[0]
+    indices = create_rolling_indices(num_timesteps_in=num_timesteps_in,
+                                     num_timesteps_out=num_timesteps_out,
+                                     n_timesteps=n_timesteps,
+                                     fix_start=fix_start)
+    
+    # use rolling window indices to subset data
+    window_features, window_target = [], []
+    for i, j in indices:
+        window_features.append(features[i:j, :])
+        window_target.append(target[(i + 1):(j + num_timesteps_out), :])
+
+    if drop_last:
+        window_features = window_features[:-1]
+        window_target = window_target[:-1]
+
+    return torch.stack(window_features), torch.stack(window_target)
 
 def create_rolling_window_ts(target, features, num_timesteps_in, num_timesteps_out, fix_start=False, drop_last=True):
     """"
