d7a3e43f516e44f7ebcb505ef26ccc2397c88824
dcuoliveira <dan.cunhaa@gmail.com>
2023-07-28 06:50:56 -0300

    add years

--- src/utils/conn_data.py
+++ src/utils/conn_data.py
@@ -11,7 +11,7 @@ def save_csv_result_in_blocks(df, args, path):
         os.makedirs(path)
 
     df["date"] = pd.to_datetime(df["date"])
-    years = list(pd.Series([dtref.year for dtref in df["date"]]).unique())
+    years = [2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021]
 
     for y in tqdm(years, total=len(years), desc="Saving Results"):
         
