5cdbffac6faf70713465156b9ce2fbbea8611a27
dcuoliveira <dan.cunhaa@gmail.com>
2023-08-16 17:40:45 -0300

    add proper prints

--- src/run_md.py
+++ src/run_md.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
     all_years = args.all_years
     long_only = args.long_only
 
-    print(use_sample_data, all_years)
+    print(use_sample_data, all_years, long_only)
 
     model_name = "{model_name}_lo".format(model_name=model_name) if long_only else "{model_name}_ls".format(model_name=model_name)
     model_name = "{}_sample".format(model_name) if args.use_sample_data else model_name
