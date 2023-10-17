0cf2a218305f9ebfe33b6c7b38bce1ac46d23b78
dcuoliveira <dan.cunhaa@gmail.com>
2023-10-16 21:17:04 -0300

    fix parameters mvo

--- src/run_mvo.py
+++ src/run_mvo.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 
 from models.MVO import MVO
 from data.CRSPSimple import CRSPSimple
-from utils.dataset_utils import create_rolling_window_ts
+from utils.dataset_utils import create_rolling_window_ts, check_bool
 from loss_functions.SharpeLoss import SharpeLoss
 from utils.conn_data import save_result_in_blocks
 
@@ -16,15 +16,16 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-mn', '--model_name', type=str, help='model name to be used for saving the model', default="mvo")
 parser.add_argument('-nti', '--num_timesteps_in', type=int, help='size of the lookback window for the time series data', default=252 * 3)
 parser.add_argument('-nto', '--num_timesteps_out', type=int, help='size of the lookforward window to be predicted', default=1)
-parser.add_argument('-usd', '--use_sample_data', type=bool, help='use sample stocks data', default=True)
-parser.add_argument('-ay', '--all_years', type=bool, help='use all years to build dataset', default=False)
-parser.add_argument('-lo', '--long_only', type=bool, help='consider long only constraint on the optimization', default=False)
+parser.add_argument('--use_small_data', type=str, help='use small sample stocks data', default="False")
+parser.add_argument('-usd', '--use_sample_data', type=str, help='use sample stocks data', default="True")
+parser.add_argument('-ay', '--all_years', type=str, help='use all years to build dataset', default="False")
+parser.add_argument('-lo', '--long_only', type=str, help='consider long only constraint on the optimization', default="False")
+parser.add_argument('-meane', '--mean_estimator', type=str, help='name of the estimator to be used for the expected returns', default="mle")
+parser.add_argument('-cove', '--covariance_estimator', type=str, help='name of the estimator to be used for the covariance of the returns', default="mle")
 
 if __name__ == "__main__":
 
     args = parser.parse_args()
-
-    print("Running script with the following parameters: model_name: {}, use_sample_data: {}, all_years: {}, long_only: {}".format(args.model_name, args.use_sample_data, args.all_years, args.long_only))
 
     model_name = args.model_name
     train_ratio = 0.6
@@ -32,19 +33,37 @@ if __name__ == "__main__":
     num_timesteps_out = args.num_timesteps_out
     fix_start = False
     drop_last = True
-    use_sample_data = args.use_sample_data
-    all_years = args.all_years
-    long_only = args.long_only
-
-    model_name = "{model_name}_lo".format(model_name=model_name) if long_only else "{model_name}_ls".format(model_name=model_name)
-    model_name = "{}_sample".format(model_name) if args.use_sample_data else model_name
+    use_small_data = check_bool(args.use_small_data)
+    use_sample_data = check_bool(args.use_sample_data)
+    all_years = check_bool(args.all_years)
+    long_only = check_bool(args.long_only)
+    mean_estimator = args.mean_estimator
+    covariance_estimator = args.covariance_estimator
     
+    print("Running script with the following parameters: model_name: {}, use_small_data {}, use_sample_data: {}, all_years: {}, long_only: {}, mean_estimator: {}, covariance_estimator: {}".format(model_name, use_small_data, use_sample_data, all_years, long_only, mean_estimator, covariance_estimator))
+
+    # add tag for long only or long-short portfolios
+    model_name = "{model_name}_lo".format(model_name=model_name) if long_only else "{model_name}_ls".format(model_name=model_name)
+
+    model_name = "{}_small".format(model_name) if use_small_data else model_name
+
+    # add tag for sample data
+    model_name = "{}_sample".format(model_name) if use_sample_data else model_name
+
+    # add mean estimator tag to name
+    model_name = "{model_name}_{mean_estimator}".format(model_name=model_name, mean_estimator=mean_estimator)
+
+    # add covariance estimator tag to name
+    model_name = "{model_name}_{covariance_estimator}".format(model_name=model_name, covariance_estimator=covariance_estimator)
+    
+    args.model_name = model_name
+
     # relevant paths
     source_path = os.path.dirname(__file__)
     inputs_path = os.path.join(source_path, "data", "inputs")
 
     # prepare dataset
-    loader = CRSPSimple(use_sample_data=use_sample_data, all_years=all_years)
+    loader = CRSPSimple(use_small_data=use_small_data, use_sample_data=use_sample_data, all_years=all_years)
     returns = loader.returns.T
     features = loader.features
     features = features.reshape(features.shape[0], features.shape[1] * features.shape[2]).T  
@@ -57,7 +76,8 @@ if __name__ == "__main__":
                                                       drop_last=drop_last)
 
     # (1) call model
-    model = MVO()
+    model = MVO(mean_estimator=mean_estimator,
+                covariance_estimator=covariance_estimator)
 
     # (2) loss fucntion
     lossfn = SharpeLoss()
@@ -102,6 +122,8 @@ if __name__ == "__main__":
     results = {
         
         "model": model,
+        "means": torch.vstack(model.means),
+        "covs": torch.stack(model.covs),
         "train_loss": None,
         "eval_loss": None,
         "test_loss": test_loss,
